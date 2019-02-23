1f75c3470119e8f59794673561e8e27c66e16236
kotaro <kotaro@Kotaro-MBP13.local>
2019-02-23 12:04:01 +0900

    Second Commit

--- ABC086C.py
+++ ABC086C.py
@@ -1,10 +1,20 @@
 n = int(input())
+t1 = 0
+x1 = 0
+y1 = 0
+cnt = 0
+
 for i in range(n):
     t, x, y = map(int, input().split())
-    if t >= (x + y) and (x - y) % 2 == 0:
-        print('OK')
+    if t - t1  >= (x - x1 + y - y1) and (x - x1 + y - y1 - t + t1) % 2 == 0:
+        t1 = t
+        x1 = x
+        y1 = y
+        cnt += 1
+        if cnt == n:
+            print('Yes')
+        else:
+            continue
     else:
-        print('NO')
-print(t)
-print(x)
-print(y)
+        print('No')
+        exit()
---
+++ test.py
@@ -0,0 +1,7 @@
+x = 8
+y = 10
+
+if (x - y) % 2 == 0:
+  print('OK')
+else:
+  print('NO')
